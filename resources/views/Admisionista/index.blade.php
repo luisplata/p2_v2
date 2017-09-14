c037bffdda994de44ccd0410c34245310048faa5
luis plata <www.luisplata@gmail.com>
2017-09-14 17:23:42 -0500

    Se hicieron las correcciones del paciente NN

--- resources/views/Admisionista/index.blade.php
+++ resources/views/Admisionista/index.blade.php
@@ -5,24 +5,29 @@
 @endsection
 
 @section("contenido")
-<h2>Registro de Pacientes *</h2>
-{{Form::open(array("url"=>"admisionista/registrarPaciente"))}}
+<h2>Registro de Pacientes * <button class="right btn btn-default" onclick="nn()">N/N</button></h2>
+{{Form::open(array("url"=>"admisionista/registrarPaciente", "id"=>"formulario"))}}
 <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-    <input type="text" class="form-control has-feedback-left" id="inputSuccess2" name="nombre" placeholder="Nombre" required />
+    <input type="text" class="form-control has-feedback-left" id="paciente_nombre" name="nombre" placeholder="Nombre" required />
     <span class="fa fa-user form-control-feedback left" aria-hidden="true"></span>
 </div>
 <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-    <input type="number" max="9999999999" class="form-control has-feedback-left" id="inputSuccess2" placeholder="Cedula" name="cedula" required />
+    <input type="number" max="9999999999" class="form-control has-feedback-left" id="paciente_cedula" placeholder="Cedula" name="cedula" required />
     <span class="glyphicon glyphicon-credit-card form-control-feedback left" aria-hidden="true"></span>
 </div>
 <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-    <input type="number" max="9999999999" class="form-control has-feedback-left" id="inputSuccess2" placeholder="Telefono" name="telefono" required />
+    <input type="number" max="9999999999" class="form-control has-feedback-left" id="paciente_telefono" placeholder="Telefono" name="telefono" required />
     <span class="fa fa-phone form-control-feedback left" aria-hidden="true"></span>
 </div>
 <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-    <input type="text" class="form-control has-feedback-left" id="inputSuccess2" placeholder="direccion" name="direccion" required />
+    <input type="text" class="form-control has-feedback-left" id="paciente_direccion" placeholder="direccion" name="direccion" required />
     <span class="glyphicon glyphicon-map-marker form-control-feedback left" aria-hidden="true"></span>
 </div>
+<div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
+    <input type="number" min="1" max="200" class="form-control has-feedback-left" id="paciente_edad" placeholder="edad" name="edad" required />
+    <span class="glyphicon glyphicon-map-marker form-control-feedback left" aria-hidden="true"></span>
+</div>
+<div class="clearfix"></div>
 <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
     <label>Grupo y RH *</label>
     <select class="form-control required" required="required" name="tipo_sangre">
@@ -57,37 +62,37 @@
     <span class="form-control-feedback left" aria-hidden="true"></span>
 </div>
 <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-    <input type="text" class="form-control has-feedback-left" id="inputSuccess2" placeholder="Nombre Procedimiento" name="nombre_proce">
+    <input type="text" class="form-control has-feedback-left" id="inputSuccess5" placeholder="Nombre Procedimiento" name="nombre_proce">
     <span class="glyphicon glyphicon-copy form-control-feedback left" aria-hidden="true"></span>
 </div>
 <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-    <input type="text" class="form-control has-feedback-left" id="inputSuccess2" placeholder="Alergias" name="alergias">
+    <input type="text" class="form-control has-feedback-left" id="inputSuccess6" placeholder="Alergias" name="alergias">
     <span class="glyphicon glyphicon-copy form-control-feedback left" aria-hidden="true"></span>
 </div>
 <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-    <input type="text" class="form-control has-feedback-left" id="inputSuccess2" placeholder="Antecedentes familiares" name="ant_familiares">
+    <input type="text" class="form-control has-feedback-left" id="inputSuccess7" placeholder="Antecedentes familiares" name="ant_familiares">
     <span class="glyphicon glyphicon-copy form-control-feedback left" aria-hidden="true"></span>
 </div>
 <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-    <input type="text" class="form-control has-feedback-left" id="inputSuccess2" placeholder="Antecedentes personales" name="ant_personales">
+    <input type="text" class="form-control has-feedback-left" id="inputSuccess8" placeholder="Antecedentes personales" name="ant_personales">
     <span class="glyphicon glyphicon-copy form-control-feedback left" aria-hidden="true"></span>
 </div>
 <div class="clearfix"></div>
 <h2>Acompañante</h2>
 <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-    <input type="text" class="form-control has-feedback-left" id="inputSuccess2" placeholder="Nombre Acompañante" name="acompaniante_nombre">
+    <input type="text" class="form-control has-feedback-left" id="inputSuccess9" placeholder="Nombre Acompañante" name="acompaniante_nombre">
     <span class="fa fa-user form-control-feedback left" aria-hidden="true"></span>
 </div>
 <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-    <input type="text" class="form-control has-feedback-left" id="inputSuccess2" placeholder="Cedula Acompañante" name="acompaniante_cedula">
+    <input type="text" class="form-control has-feedback-left" id="inputSuccess10" placeholder="Cedula Acompañante" name="acompaniante_cedula">
     <span class="glyphicon glyphicon-credit-card form-control-feedback left" aria-hidden="true"></span>
 </div>
 <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-    <input type="text" class="form-control has-feedback-left" id="inputSuccess2" placeholder="Dirección Acompañante" name="acompaniante_direccion">
+    <input type="text" class="form-control has-feedback-left" id="inputSuccess11" placeholder="Dirección Acompañante" name="acompaniante_direccion">
     <span class="glyphicon glyphicon-map-marker form-control-feedback left" aria-hidden="true"></span>
 </div>
 <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
-    <input type="text" class="form-control has-feedback-left" id="inputSuccess2" placeholder="Teléfono Acompañante" name="acompaniante_telefono">
+    <input type="text" class="form-control has-feedback-left" id="inputSuccess12" placeholder="Teléfono Acompañante" name="acompaniante_telefono">
     <span class="fa fa-phone form-control-feedback left" aria-hidden="true"></span>
 </div>
 <div class="col-md-6 col-sm-6 col-xs-12 form-group has-feedback">
@@ -136,9 +141,30 @@
 
 @section("plugin-js")
 <script type="text/javascript" src="http://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+<script type="text/javascript" src="{{asset("js/moment/moment.min.js")}}"></script>
 <script>
-$(document).ready(function () {
-    $('table').DataTable();
-});
+    $(document).ready(function () {
+        $('table').DataTable();
+    });
+    function nn() {
+        //Cargamos datos genericos y el documento random
+        var nombre = "NN";
+        var cedula = generarDocumentoRadom();
+        var telefono = "00";
+        var direccion = "NN";
+        var edad = "1";
+        $("#paciente_nombre").val(nombre);
+        $("#paciente_cedula").val(cedula);
+        $("#paciente_telefono").val(telefono);
+        $("#paciente_direccion").val(direccion);
+        $("#paciente_edad").val(edad);
+
+    }
+    function generarDocumentoRadom() {
+        //documento random con formato de [fecha][random4digitos]
+        var fecha = moment().format('DDMMYYYY');
+        var consecutivo = Math.round(Math.random() * (9999 - 1) + 1);
+        return fecha + consecutivo + "";
+    }
 </script>
 @endsection
