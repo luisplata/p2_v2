7b379faf8e7e3171c4af4b9ba356300c106128bb
luis plata <www.luisplata@gmail.com>
2017-10-25 10:10:47 -0500

    Se modifico el simulador para que envie datos normales y cada minuto envie datos fuera de lo normal, para mostrar las alertas medicas

--- resources/views/simulador.blade.php
+++ resources/views/simulador.blade.php
@@ -8,9 +8,9 @@ simulador
 <script src="http://stevenlevithan.com/assets/misc/date.format.js"></script>
 <script>
     function contador(){
-        var pulso = Math.floor(Math.random() * (101 - (49))) + (49); //(MAX - (MIN))) + (MIN);
+        var pulso = Math.floor(Math.random() * (100 - (50))) + (50); //(MAX - (MIN))) + (MIN);
         var cubiculo = Math.floor(Math.random() * (7 - (1))) + (1);
-        var oxigeno = Math.floor(Math.random() * (220 - (90))) + (90);
+        var oxigeno = Math.floor(Math.random() * (220 - (95))) + (95);
 
         var Arduino = new Object();
         Arduino.pulso = pulso;
@@ -27,8 +27,21 @@ simulador
 
     }
     
-    function Revisor() {
-        
+    function mandandoFueraDeLoNormal() {
+        setInterval(function(){
+			var Arduino = new Object();
+			Arduino.pulso = 120;
+			Arduino.cubiculo = 1;
+			Arduino.oxigeno = 90;
+			Arduino.fecha = new Date();
+
+			//console.log(Arduino.fecha.format("yyyy-mm-dd-hh:MM:ss"));
+
+			console.log(Arduino);
+			$.ajax({
+				url: "/simulador/recepcion/"+Arduino.cubiculo+"/"+Arduino.fecha.format("yyyy-mm-dd-hh:MM:ss")+"/"+Arduino.pulso+"/"+Arduino.oxigeno
+			});
+		},60000);
     }
 
     $(document).ready(function(){
